3b04f13d2bbac5d7e89a7443d337cd5c5084876b
Homayoun <homayoun763@gmail.com>
2024-04-06 00:26:19 +0330

    Trusted

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import Companies from './components/Companies'
 import Features from './components/Features'
+import Trusted from './components/Trusted'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Companies />
       <Features />
+      <Trusted />
     </div>
   )
 }
