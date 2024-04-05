6eab4abee2776e05441eb8c08011a17d1ff5c67e
Homayoun <homayoun763@gmail.com>
2024-04-06 00:20:12 +0330

    features

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Companies from './components/Companies'
+import Features from './components/Features'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Companies />
+      <Features />
     </div>
   )
 }
